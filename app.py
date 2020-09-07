d436e78e29289e38270a456f998c86a34937dde9
vinaynpp <65890217+vinaynpp@users.noreply.github.com>
2020-09-07 22:50:45 +0530

    upgrade

--- app.py
+++ app.py
@@ -1,21 +1,66 @@
 import os
-import time
-
-import yagmail
-import keyring
 
 import mark1
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
+from checknaka import dekh
+from flask import url_for
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
+@app.route('/portal/', methods=['GET', 'POST'])
+def takeform():
+    if request.method == 'GET':
+        return render_template('portal.html')
+    name = request.form['name']
     # Retrieve the name from url parameter
+    meraemail = request.form['meraemail']
+    merapswd = request.form['merapswd']
+    email = request.form['email']
+    subject = request.form['subject']
+    contents = request.form['contents']
+
+    # For debugging
+
+    print(f"got name {name}")
+    print(f"got meraemail {meraemail}")
+    print(f"got merapswd {merapswd}")
+    print(f"got email {email}")
+    print(f"got subject {subject}")
+    print(f"got contents {contents}")
+
+    response = {}
+    response["name"] = f'{dekh(name)}'
+    response["meraemail"] = f'{dekh(meraemail)}'
+    response["merapswd"] = f'{dekh(merapswd)}'
+    response["email"] = f'{dekh(email)}'
+    response["subject"] = f'{dekh(subject)}'
+    response["contents"] = dekh(contents)
 
 
+    # Check if user sent a name at all
+    if not meraemail:
+        response["ERROR"] = "no password found, please send a password."
+    # Check if the user entered a number not a name
+    elif str(email).isdigit():
+        response["ERROR"] = f"{email}can't be numeric."
+    # Now the user entered a valid name
+    else:
+        mark1.bhejo(meraemail, merapswd, email, subject, contents)
+
+        response["MESSAGE"] = f"Welcome {meraemail} with password {merapswd} to our awesome platform!!"
+
+    # Return the response in json format
+
+    return jsonify(response)
+
+
+@app.route('/json/', methods=['GET', 'POST'])
+def takejson():
+
+    name = request.args.get("name", None)
     meraemail = request.args.get("meraemail", None)
     merapswd = request.args.get("merapswd", None)
     email = request.args.get("email", None)
@@ -24,20 +69,27 @@ def respond():
 
     # For debugging
 
-    print(f"got name {meraemail}")
-    print(f"got name {merapswd}")
-    print(f"got name {email}")
-    print(f"got name {subject}")
-    print(f"got name {contents}")
+    print(f"got name {name}")
+    print(f"got meraemail {meraemail}")
+    print(f"got merapswd {merapswd}")
+    print(f"got email {email}")
+    print(f"got subject {subject}")
+    print(f"got contents {contents}")
 
     response = {}
+    response["name"] = f'{dekh(name)}'
+    response["meraemail"] = f'{dekh(meraemail)}'
+    response["merapswd"] = f'{dekh(merapswd)}'
+    response["email"] = f'{dekh(email)}'
+    response["subject"] = f'{dekh(subject)}'
+    response["contents"] = dekh(contents)
 
     # Check if user sent a name at all
     if not meraemail:
         response["ERROR"] = "no password found, please send a password."
     # Check if the user entered a number not a name
     elif str(email).isdigit():
-        response["ERROR"] = "{email}can't be numeric."
+        response["ERROR"] = f"{email}can't be numeric."
     # Now the user entered a valid name
     else:
         mark1.bhejo(meraemail, merapswd, email, subject, contents)
---
+++ checknaka.py
@@ -0,0 +1,7 @@
+
+
+def dekh(input):
+    if not input:
+        return str(input)
+    else:
+        return str(input)
